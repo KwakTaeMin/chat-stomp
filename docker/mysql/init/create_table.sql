299c7a0e0eb0504e5e03a9b21eb39c39b9555916
niKe <taminging@kakao.com>
2023-07-26 00:10:13 +0900

    docker mysql 셋팅 create_table.sql 생성

---
+++ docker/mysql/init/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `user` (
+     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '사용자 Primary Key',
+     `email` varchar(64) NOT NULL COMMENT '사용자 로그인용 ID',
+     `provider` varchar(200) NOT NULL COMMENT '사용자 인증 서비스 사',
+     `nickname` varchar(200) NOT NULL COMMENT '사용자 이름',
+     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
+     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일',
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `u_idx_1` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COMMENT='사용자 테이블';
