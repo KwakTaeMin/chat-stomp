74c40086aa230311b6282534e48758f68c7ddccf
niKe <taminging@kakao.com>
2023-07-30 22:49:14 +0900

    README.md

--- src/main/java/com/taemin/chat/config/SecurityConfig.java
+++ src/main/java/com/taemin/chat/config/SecurityConfig.java
@@ -38,8 +38,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));//front-end cors 허용
+        configuration.setAllowedMethods(Arrays.asList("GET","POST")); // get, post 만
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
